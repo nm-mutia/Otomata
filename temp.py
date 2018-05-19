384fbc00e427fefa8367746d1e851b187e53e79d
Unknown <rizvisfbrina@gmail.com>
2018-05-19 14:34:31 +0700

    aaa

--- temp.py
+++ temp.py
@@ -8,6 +8,18 @@ This is a temporary script file.
 from html.parser import HTMLParser
 import codecs
 
+reserve = ['false','class','finally','is','return'
+           ,'none','continue','for','lambda','try'
+           ,'true','def','from','nonlocal','while'
+           ,'and','del','global','not','with','as'
+           ,'elif','if','or','yield','assert','else'
+           ,'import','pass','break','except','in','raise']
+
+operator = ['+', '-', '*', '**', '/',
+            '//', '%', '@', '<<', '>>',
+            '&', '|', '^', '~','<','>',
+            '<=','>=','==','!=']
+
 # create a subclass and override the handler methods
 class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
@@ -19,19 +31,15 @@ class MyHTMLParser(HTMLParser):
         print ("Encountered an end tag :", tag)
 
     def handle_data(self, data):
-        print("Encountered some data  :", data)
+        if data == reserve :
+            print("Reserved Word : ", data)
+        elif data == operator :
+            print("Operator : ", data)
+        else :
+            print("Encountered some data  :", data)
 
 
-reserve = ['false','class','finally','is','return'
-           ,'none','continue','for','lambda','try'
-           ,'true','def','from','nonlocaal','while'
-           ,'and','del','global','not','with','as'
-           ,'elif','if','or','yield','assert','else'
-           ,'import','pass','break','except','in','raise']
-operator = ['+', '-', '*', '**', '/',
-            '//', '%', '@', '<<', '>>',
-            '&', '|', '^', '~','<','>',
-            '<=','>=','==','!=']
+
 # instantiate the parser and fed it some HTML
 parser = MyHTMLParser()
 f = codecs.open("test.html", 'r')
