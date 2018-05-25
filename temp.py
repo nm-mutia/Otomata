bcb6c3cc337c8c846321a55b9904b9f1b02fca1b
Unknown <nm.mutia02@gmail.com>
2018-05-25 10:38:33 +0700

    Auto stash before merge of "master" and "origin/master"

--- temp.py
+++ temp.py
@@ -1,24 +1,32 @@
-# -*- coding: utf-8 -*-
 from html.parser import HTMLParser
-import codecs
 
-operator = ['+', '-', '*', '**', '/',
-            '//', '%', '@', '<<', '>>',
-            '&', '|', '^', '~','<','>',
-            '<=','>=','==','!=']
+total = 0
+startag = 0
+endtag = 0
+datax = 0
+comment = 0
+decl = 0
+attrb = 0
 
-# create a subclass and override the handler methods
 class MyHTMLParser(HTMLParser):
     def handle_decl(self, data):
         print("Decl \t\t:", data)
+        global decl
+        decl = decl + 1
         
     def handle_starttag(self, tag, attrs):
         print ("Start tag \t:", tag)
+        global startag
+        global attrb
+        startag = startag + 1
         for attr in attrs:
             print("attr\t\t:", attr)
+            attrb = attrb + 1
 
     def handle_endtag(self, tag):
         print ("End tag \t:", tag)
+        global endtag
+        endtag = endtag + 1
 
     def handle_data(self, data):
         if data == '\n' :
@@ -27,14 +35,33 @@ class MyHTMLParser(HTMLParser):
             return
         else :
             print("Some data \t:", data)
+            global datax
+            datax = datax + 1
             
     def handle_comment(self, data):
         print("Comment \t:", data)
+        global comment
+        comment = comment + 1
 
+           
 
-# instantiate the parser and fed it some HTML
 parser = MyHTMLParser()
-f = codecs.open("test.html", 'r')
+f = open("test.html", 'r') 
 parser.feed(f.read())
-#parser.feed('<html><head><title>Test</title></head>'
-           # '<body><h1>Parse me!</h1></body></html>')
+
+total = decl + startag + attrb + datax + endtag + comment
+
+print ('\n')
+print (' - decl tag : ', decl)
+print (' - start tag : ', startag)
+print (' - attr : ', attrb)
+print (' - data : ', datax)
+print (' - end tag : ', endtag)
+print (' - comment : ', comment)
+print (' - total : ', total)
+        
+        
+        
+        
+        
+        
